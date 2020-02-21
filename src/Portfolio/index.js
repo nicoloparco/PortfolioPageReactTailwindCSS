6c89491de4bc79a1ad2310a3043fceb887511ce4
Nico Loparco <nicoloparco75@gmail.com>
2020-02-21 18:49:14 -0500

    added images for projects

--- src/Portfolio/index.js
+++ src/Portfolio/index.js
@@ -1,7 +1,16 @@
 import React, {Component} from "react";
 import ProjectCard from "../ProjectCard";
-import projects from "../projects.json"
-import Sports from "../Assets/Sports.jpg"
+import projects from "../projects.json";
+import Sports from "../Assets/Sports.png";
+import Burger from "../Assets/Burger.png";
+import News from "../Assets/News.jpg"
+import Crystal from "../Assets/CrystalGame.png";
+import Friends from "../Assets/Friends.png";
+import Shopper from "../Assets/Shopper.png";
+import LIRI from "../Assets/LIRI.png";
+import Trivia from "../Assets/TriviaGame.png";
+import GIPHY from "../Assets/GIPHY.png";
+
 
 class Portfolio extends Component {
 
@@ -16,6 +25,7 @@ class Portfolio extends Component {
                 description={projects[0].description}
                 techUsed={projects[0].techUsed}
                 githubLink={projects[0].githubLink}
+                deployedLink={projects[0].deployedLink}
                 image={Sports}
                />
                <ProjectCard
@@ -23,56 +33,61 @@ class Portfolio extends Component {
                 description={projects[1].description}
                 techUsed={projects[1].techUsed}
                 githubLink={projects[1].githubLink}
-                image={Sports}
+                deployedLink={projects[1].deployedLink}
+                image={Burger}
                />
                <ProjectCard
                 name={projects[2].name}
                 description={projects[2].description}
                 techUsed={projects[2].techUsed}
                 githubLink={projects[2].githubLink}
-                image={Sports}
+                image={News}
                />
                <ProjectCard
                 name={projects[3].name}
                 description={projects[3].description}
                 techUsed={projects[3].techUsed}
                 githubLink={projects[3].githubLink}
-                image={Sports}
+                deployedLink={projects[3].deployedLink}
+                image={Crystal}
                />
                <ProjectCard
                 name={projects[4].name}
                 description={projects[4].description}
                 techUsed={projects[4].techUsed}
                 githubLink={projects[4].githubLink}
-                image={Sports}
+                deployedLink={projects[4].deployedLink}
+                image={Friends}
                />
                <ProjectCard
                 name={projects[5].name}
                 description={projects[5].description}
                 techUsed={projects[5].techUsed}
                 githubLink={projects[5].githubLink}
-                image={Sports}
+                image={Shopper}
                />
                <ProjectCard
                 name={projects[6].name}
                 description={projects[6].description}
                 techUsed={projects[6].techUsed}
                 githubLink={projects[6].githubLink}
-                image={Sports}
+                image={LIRI}
                />
                <ProjectCard
                 name={projects[7].name}
                 description={projects[7].description}
                 techUsed={projects[7].techUsed}
                 githubLink={projects[7].githubLink}
-                image={Sports}
+                deployedLink={projects[7].deployedLink}
+                image={Trivia}
                />
                <ProjectCard
                 name={projects[8].name}
                 description={projects[8].description}
                 techUsed={projects[8].techUsed}
                 githubLink={projects[8].githubLink}
-                image={Sports}
+                deployedLink={projects[8].deployedLink}
+                image={GIPHY}
                />
             </div>
         )
--- src/ProjectCard/index.js
+++ src/ProjectCard/index.js
@@ -5,7 +5,7 @@ function ProjectCard(props) {
         <div className="rounded-lg shadow-xl mx-4 mb-4 mt-6 overflow-hidden lg:flex">
             <img  className="lg:h-64 lg:rounded-lg" src={props.image} alt={props.name}></img>
             <div className="px-4 py-4">
-                <h3 className="font-semibold justify-start">{props.name}</h3>
+                <a href={props.deployedLink} class="hover:text-gray-800"><h3 className="font-semibold justify-start">{props.name}</h3></a>
                 <p className="text-xs my-4">{props.description}</p>
                 <p className="text-xs my-4 font-semibold italic">{props.techUsed}</p>
                 <a className="text-xs my-4 text-white text-right" href={props.githubLink}>GitHub Repo</a>
