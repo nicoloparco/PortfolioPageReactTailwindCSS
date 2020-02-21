42aa0fc2e2aaa51c42bfeb0238fca95874351193
Nico Loparco <nicoloparco75@gmail.com>
2020-02-21 17:51:56 -0500

    Updates

---
+++ precache-manifest.e9a4d3e9c3d2f3a87c6fbf0233076756.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "461b751dfca0363e6b7481a44c690458",
+    "url": "/PortfolioPageReactTailwindCSS/index.html"
+  },
+  {
+    "revision": "bbeff9962c145243f21b",
+    "url": "/PortfolioPageReactTailwindCSS/static/css/main.e22c8e25.chunk.css"
+  },
+  {
+    "revision": "c4d992815ee9dced23ae",
+    "url": "/PortfolioPageReactTailwindCSS/static/js/2.c7addd1b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/PortfolioPageReactTailwindCSS/static/js/2.c7addd1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbeff9962c145243f21b",
+    "url": "/PortfolioPageReactTailwindCSS/static/js/main.3f068f5d.chunk.js"
+  },
+  {
+    "revision": "5ac0580e73c8edbc8754",
+    "url": "/PortfolioPageReactTailwindCSS/static/js/runtime-main.4afc9833.js"
+  },
+  {
+    "revision": "818b95764f1490cebc869ab3fd375b7e",
+    "url": "/PortfolioPageReactTailwindCSS/static/media/Sports.818b9576.jpg"
+  }
+]);
