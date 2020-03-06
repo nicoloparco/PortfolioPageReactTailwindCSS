137a95991a357c2db7e7ca168dbe69dada607330
Nico Loparco <nicoloparco75@gmail.com>
2020-03-06 16:11:49 -0500

    added project

--- src/Portfolio/index.js
+++ src/Portfolio/index.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import ProjectCard from "../ProjectCard";
 import projects from "../projects.json";
 import Sports from "../Assets/Sports.png";
+import Burger2 from "../Assets/Burger2.png";
 import Burger from "../Assets/Burger.png";
 import News from "../Assets/News.jpg"
 import Crystal from "../Assets/CrystalGame.png";
@@ -10,6 +11,7 @@ import Shopper from "../Assets/Shopper.png";
 import LIRI from "../Assets/LIRI.png";
 import Trivia from "../Assets/TriviaGame.png";
 import GIPHY from "../Assets/GIPHY.png";
+import Hangman from "../Assets/Hangman.png";
 
 
 class Portfolio extends Component {
@@ -34,22 +36,22 @@ class Portfolio extends Component {
                 techUsed={projects[1].techUsed}
                 githubLink={projects[1].githubLink}
                 deployedLink={projects[1].deployedLink}
-                image={Burger}
+                image={Burger2}
                />
                <ProjectCard
                 name={projects[2].name}
                 description={projects[2].description}
                 techUsed={projects[2].techUsed}
                 githubLink={projects[2].githubLink}
-                image={News}
+                deployedLink={projects[2].deployedLink}
+                image={Burger}
                />
                <ProjectCard
                 name={projects[3].name}
                 description={projects[3].description}
                 techUsed={projects[3].techUsed}
                 githubLink={projects[3].githubLink}
-                deployedLink={projects[3].deployedLink}
-                image={Crystal}
+                image={News}
                />
                <ProjectCard
                 name={projects[4].name}
@@ -57,29 +59,29 @@ class Portfolio extends Component {
                 techUsed={projects[4].techUsed}
                 githubLink={projects[4].githubLink}
                 deployedLink={projects[4].deployedLink}
-                image={Friends}
+                image={Crystal}
                />
                <ProjectCard
                 name={projects[5].name}
                 description={projects[5].description}
                 techUsed={projects[5].techUsed}
                 githubLink={projects[5].githubLink}
-                image={Shopper}
+                deployedLink={projects[5].deployedLink}
+                image={Friends}
                />
                <ProjectCard
                 name={projects[6].name}
                 description={projects[6].description}
                 techUsed={projects[6].techUsed}
                 githubLink={projects[6].githubLink}
-                image={LIRI}
+                image={Shopper}
                />
                <ProjectCard
                 name={projects[7].name}
                 description={projects[7].description}
                 techUsed={projects[7].techUsed}
                 githubLink={projects[7].githubLink}
-                deployedLink={projects[7].deployedLink}
-                image={Trivia}
+                image={LIRI}
                />
                <ProjectCard
                 name={projects[8].name}
@@ -87,7 +89,23 @@ class Portfolio extends Component {
                 techUsed={projects[8].techUsed}
                 githubLink={projects[8].githubLink}
                 deployedLink={projects[8].deployedLink}
+                image={Trivia}
+               />
+               <ProjectCard
+                name={projects[9].name}
+                description={projects[9].description}
+                techUsed={projects[9].techUsed}
+                githubLink={projects[9].githubLink}
+                deployedLink={projects[9].deployedLink}
                 image={GIPHY}
+                />
+               <ProjectCard
+                name={projects[10].name}
+                description={projects[10].description}
+                techUsed={projects[10].techUsed}
+                githubLink={projects[10].githubLink}
+                deployedLink={projects[10].deployedLink}
+                image={Hangman}
                />
             </div>
         )
